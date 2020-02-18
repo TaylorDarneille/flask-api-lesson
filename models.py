37d76b1e11314465f6a61067a1e6cec43fd60984
Taylor Darneille <tmdarneille@gmail.com>
2020-02-18 11:46:09 -0800

    created Dog Model, configured DB connection

--- models.py
+++ models.py
@@ -15,6 +15,28 @@ DATABASE = SqliteDatabase('dogs.sqlite')
 
 
 # defining our Dog model
-# not Dog inherits from Model, this gives us methods to do CRUD actions and to define attribute/field/column names and types
+# note Dog inherits from Model, this gives us methods to do CRUD actions and to define attribute/field/column names and types
 # see here: http://docs.peewee-orm.com/en/latest/peewee/models.html#
 # http://docs.peewee-orm.com/en/latest/peewee/models.html#fields
+class Dog(Model):
+	name = CharField() # string
+	owner = CharField() # string for now, later we will implement a relation
+	breed = CharField()
+	# this is how you specify default values
+	created_at = DateTimeField(default=datetime.datetime.now) #mistake on purpose
+
+	# special constructor that gives our class instructions on how to connect to a specific database
+	class Mega:
+		database = DATABASE # use the DB defined above as DATABASE for this model
+
+# define a method that will get called in app.py to set up DBs/connection
+# similar to how we ran /db/db.js in server.js in express unit
+def initialize(): # note we are making this name up
+	DATABASE.connect() # analogous to mongoose.connect(...)
+
+	# we need to explicitly create the tables based on the schema definitions above
+	DATABASE.create_tables([Dog], safe=True)
+	print("Connected to DB and created tables if they weren't already there")
+
+	# with SQL, dont' leave DB connection open, we dont want to hog up space in the connection pool
+	DATABASE.close()
