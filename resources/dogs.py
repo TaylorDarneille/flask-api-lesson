5b0c432d112c8d4d71dd2efd90aad8b4cbc7c61f
Taylor Darneille <tmdarneille@gmail.com>
2020-02-20 11:29:16 -0800

    filled out dog create route to send back json and status code but haven't tested yet

--- resources/dogs.py
+++ resources/dogs.py
@@ -1,10 +1,13 @@
 # this file is like dogcongtroller.js
 import models
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 # request -- data from client's request is sent to the global request object
 # we can use this object to get the json or form data or whatever
 # reassigned on every request that has a body
+
+# this is some useful extra tools that come with peewee
+from playhouse.shortcuts import model_to_dict
 
 # first arg is the blueprint's name
 # second arg is its import_name
@@ -20,5 +23,24 @@ def create_dog():
 	# .get_json() attatched to request object will extract the JSON from the request body
 	payload = request.get_json()
 	print(payload)
-	
-	return "you hit dog create route"
+
+	# us our peewee model to add something to database
+	dog = models.Dog.create(name=payload['name'], owner=payload['owner'], breed=payload['breed'])
+
+	print(dog) # just prints the ID -- check in sqlite3 on the command line
+			   # run sqlite3 dogs.sqlite to open a CLI that lets you enter the SQL queries
+
+	print(dog.__dict__) # this sometimes shows you more useful info
+						# .__dict__ is a class attribute automatically added to python classes
+
+	print(dir(dog)) # look at all the pretty methods!!!
+
+	# notice you can't directly jsonify dog since it's not a dictionary or other jsonifiable thing
+	# it causes TypeError: Object of type 'Dog' is not JSON serializable
+
+	# we can use model_to_dict from playhouse to convert the model to a dict
+
+	dog_dict=model_to_dict(dog)
+
+	# notice you can add 201 to the return and you will send a proper HTTP status cod
+	return jsonify(data=dog_dict, status={'message': 'Successfully created dog!'}), 201
