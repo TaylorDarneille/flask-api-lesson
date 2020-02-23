8f3632e04ce56422ac9c49de91208c772ae887da
Taylor Darneille <tmdarneille@gmail.com>
2020-02-23 09:07:28 -0800

    User class using UserMixin from flask-login, user table created when flask runs

--- models.py
+++ models.py
@@ -9,12 +9,27 @@ from peewee import *
 # and
 # Model -- the Model() class is what we will inherit from when defining our models (similar to using some stuff from the mongoos module when creating a schema and then a model in mongoose)
 
+# we weill use this module ot set up User model, sessions, logins, authentication, require authentication for certain things, etc.
+# the UserMixin in what we need to make our User model
+# https://flask-login.readthedocs.io/en/latest/
+from flask_login import UserMixin
+
+
 # sqlite is a way to have a "database" that's just stored in a file
 # it's great for development because you can have easily portable data (on git, eg)
 # later when deploying, we will change this to psql
 DATABASE = SqliteDatabase('dogs.sqlite')
 # analogous to MONGO_DB_URL = 'mongodb://localhost/dogs', {...}
 
+# to behave correcly in flask-login's session/login/etc functionality, the User class must have some methods and properties that Model from peewee doesn't have
+# we could write these ourselves, adn/or we could also have our User class inherit from UserMixin (in addition to pweewee's Model class), which will provide/implement them for us
+# https://flask-login.readthedocs.io.en/latest/#your-user-class
+class User(UserMixin, Model):
+	username=Charfield(unique=True)
+	email = Charfield(unique=True)
+	password = CharField()
+
+	class Meta: database=DATABASE
 
 # defining our Dog model
 # note Dog inherits from Model, this gives us methods to do CRUD actions and to define attribute/field/column names and types
@@ -39,7 +54,7 @@ def initialize(): # note we are making this name up
 	DATABASE.connect() # analogous to mongoose.connect(...)
 
 	# we need to explicitly create the tables based on the schema definitions above
-	DATABASE.create_tables([Dog], safe=True)
+	DATABASE.create_tables([User, Dog], safe=True)
 	print("Connected to DB and created tables if they weren't already there")
 
 	# with SQL, dont' leave DB connection open, we dont want to hog up space in the connection pool
