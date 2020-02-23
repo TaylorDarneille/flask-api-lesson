012ce3122f453d65ea4e421b951aa9ab25744f05
Taylor Darneille <tmdarneille@gmail.com>
2020-02-23 09:28:35 -0800

    users post (register) stubbed out and working

--- resources/users.py
+++ resources/users.py
@@ -1,7 +1,7 @@
 #this is like a "user controller" or maybe an "auth controller"
 import models
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 #maket this a blueprint
 users = Blueprint('users','users')
@@ -9,3 +9,8 @@ users = Blueprint('users','users')
 @users.route('/', methods=['GET'])
 def test_user_resource():
 	return "we have a user resource"
+
+@users.route('/register/', methods=['POST'])
+def register():
+	print(request.get_json())
+	return "check terminal"
