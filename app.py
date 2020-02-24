4f3f94a42f7f6de4510bcb4086940a5b611f0acc
Taylor Darneille <tmdarneille@gmail.com>
2020-02-24 10:45:34 -0600

    dummy push to try to fix models not defined error

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import os
 
-if 'ON_HEROKU' in os.environ:
-	print('\non heroku!')
-	models.initialize()
-
 # from flask import Flask is like const express = require('express')
 # jsonify lets us send JSON HTTP responses
 # g lets us use global variable for the life of (i.e. in the context of )
 from flask import Flask, jsonify, g
 
-# get a pakage that will let us handle CORS
+# get a package that will let us handle CORS
 # https://flask-cors.readthedocs.io.en/latest/
 from flask_cors import CORS
 
@@ -18,11 +14,12 @@ from flask_cors import CORS
 # LM is the main tool for coordinating sessions/login stuff in our app
 from flask_login import LoginManager
 
-from resources.dogs import dogs # import blueprint from ./resources/dogs
+from resources.dogs import dogs
 from resources.users import users
 # in python you import a file and you get everything in the "global scope" of that file
 # so this statement imports all variables and methods from that file as properties on the models object (e.g. models.initialize() is available here)
-# noe we did not explicitly export anything in models.py
+# note we did not explicitly export anything in models.py
+
 # google 'namespacing in python'
 import models
 
@@ -33,22 +30,17 @@ PORT = 8000
 # instantiating the Flask class
 app = Flask(__name__)
 
-#according to this
-# https://flask-login.readthedocs.io/en/latest/#configuring-your-application
-# we need to do several things
+# ------ flask-login things -------
 
 # 1. set up a secret key
-# https://flask.palletprojects.com/en/1.1.x.quickstart/#sessions
-app.secret_key = "Good Tea. Nice House. This is very secret."
-
+app.secret_key = "Equip Sunglasses"
 # 2. instantiate LoginManager to get a login_manager
 login_manager = LoginManager()
-
-#3. actually connect the app with teh login manager
+#3. actually connect the app with the login manager
 login_manager.init_app(app)
 
 # in register and login we did login_user(user that was found or created)
-# that puts the ID of tha tuser in session
+# that puts the ID of that user in session
 # to use the user object use id that is, you must define a callback for user_loader(part of login manager) to us
 # userloader will use this callback to load the user object
 # see https://flask-login.readthedocs.io.en/latest/#how-it-works
@@ -71,29 +63,16 @@ def unautheroized():
 	), 401
 
 # CORS = Cross Origin Resource Sharing
-# a web domain is an "origin"
-# This app is localhost:8000, that's an origin
-# our React app is localhouse:3000, that's a different origin
-# Browsers implement CORS to prevent a JS app from sending requests to origins other than the one the browser originally went to to get the JS
-# (i.e. say some JS tries to send data to some nefarious 3rd party)
-# configuring CORS lets browser say "here's who I'm expecting to hear from"
-# (certain origins)
 # first arg -- we add cors to blueprints
 # second arg -- list of allowed origins
 # third arg -- lets us accept requests with cookies attached, this allows us to use sessions for auth
 CORS(dogs, origins=['http://localhost:3000'], supports_credentials=True)
 CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
 
-
-# we are using blueprints to make "controllers"
-# "use this bluprint (component/piece/section of the app) to handle the dog stuff"
-# analogous to app.use('/dogs', dogController)
-# the point of the api v no is to let you build an upgraded API with a different url prefix, adn let yoru old API remain intact so that you don't break a bunch of apps already built on top of yoru old API with the old URLS
+# blueprints <> controllers
 app.register_blueprint(dogs, url_prefix='/api/v1/dogs')
 app.register_blueprint(users, url_prefix='/api/v1/users')
 
-
-# we dont want ot hog up the SQL connection pool so we should connect to the DB before every request and close the db connection after every request
 @app.before_request # use this decorator to cause a function to run before reqs
 def before_request():
 	"""Connect to the db before each request"""
@@ -107,49 +86,16 @@ def after_request(response):
 	g.db.close()
 	return response # go ahead and send the response back to client (in our case this will be some JSON)
 
-# here's how you write a route in flask
-# note the default URL ends in /
-@app.route('/') # @ symbol here means this is a decorator
+@app.route('/')
 def index():
 	return 'Hello, world!'
 
-# it's finicky abou types -- eg you can't return a list
-# check the error message at this route
-# @app.route('/test')
-# def test_list():
-# 	return ['hello', 'there']
-
-# lets use the jsonify module to try to send the lst as json
-# @app.route('/test_json')
-# def test_json():
-# 	# we are using jsonify to create a JSON response
-# 	# this is analogous to res.json() in express
-# 	return jsonify(['hello', 'there'])
-
-# @app.route('/cat_json')
-# def get_cat_json():
-# 	# you can pass key value pairs into jsonify()
-# 	return jsonify(name="Nico", age=15)
-
-# you can use a dictionary as the value of one of the key-value pairs in jsonify()
-# @app.route('/nested_json')
-# def get_nested_json():
-# 	nico = {
-# 		'name': 'Nico',
-# 		'age': 15
-# 	}
-# 	return jsonify(name="Reuben", age=14, cat=nico)
-
-# url parameters in flask look like this
-# username is teh URL param below like :username in express
-# @app.route('/say_hello/<username>')
-# def say_hello(username): # this func takes URL param as arg
-# 	return "Hello {}".format(username)
+if 'ON_HEROKU' in os.environ:
+	print('\non heroku!')
+	models.initialize()
 
 # this is like app.listen() in express -- at the bottom
-# __name__ being '__main__' here mean sthat we just ran this file as opposed to exporting it and importing it somewhere else
+# __name__ being '__main__' here means that we just ran this file as opposed to exporting it and importing it somewhere else
 if __name__=='__main__':
-	# when we start the app set up our tables (if necessary) as defined in models.py
-	# remember in express we required db.js before app.listen
-	models.initialize()
+	models.initialize() # this is defined in models.py
 	app.run(debug=DEBUG, port=PORT)
--- models.py
+++ models.py
@@ -1,18 +1,6 @@
 import os
 import datetime
-
-# all our models will go in this file
-
-# peewee is similar ot mongoose
 from peewee import *
-# import * means everything, including:
-# SqliteDatabase -- adapter that lets us connect to sqlite databases (see below)
-# and
-# Model -- the Model() class is what we will inherit from when defining our models (similar to using some stuff from the mongoos module when creating a schema and then a model in mongoose)
-
-# we weill use this module ot set up User model, sessions, logins, authentication, require authentication for certain things, etc.
-# the UserMixin in what we need to make our User model
-# https://flask-login.readthedocs.io/en/latest/
 from flask_login import UserMixin
 
 # for deployment
@@ -21,53 +9,32 @@ from playhouse.db_url import connect
 if 'ON_HEROKU' in os.environ:
 	DATABASE = connect(os.environ.get('DATABASE_URL'))
 else:
-	# sqlite is a way to have a "database" that's just stored in a file
-	# it's great for development because you can have easily portable data (on git, eg)
-	# later when deploying, we will change this to psql
 	DATABASE = SqliteDatabase('dogs.sqlite')
-	# analogous to MONGO_DB_URL = 'mongodb://localhost/dogs', {...}
 
 # to behave correcly in flask-login's session/login/etc functionality, the User class must have some methods and properties that Model from peewee doesn't have
-# we could write these ourselves, adn/or we could also have our User class inherit from UserMixin (in addition to pweewee's Model class), which will provide/implement them for us
+# we could write these ourselves, and/or we could also have our User class inherit from UserMixin (in addition to pweewee's Model class), which will provide/implement them for us
 # https://flask-login.readthedocs.io.en/latest/#your-user-class
 class User(UserMixin, Model):
 	username=CharField(unique=True)
 	email = CharField(unique=True)
 	password = CharField()
 
-	class Meta: database=DATABASE
+	class Meta:
+		database=DATABASE
 
-# defining our Dog model
-# note Dog inherits from Model, this gives us methods to do CRUD actions and to define attribute/field/column names and types
-# see here: http://docs.peewee-orm.com/en/latest/peewee/models.html#
-# http://docs.peewee-orm.com/en/latest/peewee/models.html#fields
 class Dog(Model):
 	name = CharField() # string
-	
-	# to set up our 1:M relationship between users and dogs we will need a foreign key
-	# http://docs.peewee-orm.com/en/latest/peewee/example.html#models
 	owner = ForeignKeyField(User, backref='dogs')
-	# if we had a dog model instance in a variable called some_dog, the FK will let us go some_dog.owner to get the owner
-	# and if we had a user model instance in a var called some_user, the bakref will allow us ot go some_user.dogs ot get a list of dog instances
-
 	breed = CharField()
-	# this is how you specify default values
-	created_at = DateTimeField(default=datetime.datetime.now) # mistake on purpose
-	# we often need to import things in python that might be built in in other languages
-	# this keeps python lightweight
+	created_at = DateTimeField(default=datetime.datetime.now)
 
-	# special constructor that gives our class instructions on how to connect to a specific database
 	class Meta:
-		database = DATABASE # use the DB defined above as DATABASE for this model
+		database = DATABASE
 
-# define a method that will get called in app.py to set up DBs/connection
-# similar to how we ran /db/db.js in server.js in express unit
+# this will get called in app.py
 def initialize(): # note we are making this name up
 	DATABASE.connect() # analogous to mongoose.connect(...)
-
-	# we need to explicitly create the tables based on the schema definitions above
 	DATABASE.create_tables([User, Dog], safe=True)
 	print("Connected to DB and created tables if they weren't already there")
+	DATABASE.close() # close connection so you're not taking up space
 
-	# with SQL, dont' leave DB connection open, we dont want to hog up space in the connection pool
-	DATABASE.close()
